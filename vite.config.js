357335856b7f6958930dead01c995f6164ba82a3
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-01-02 22:28:18 +0700

    feat: split chunks

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from "vite";
 import { dependencies } from "./package.json";
 import react from "@vitejs/plugin-react";
-function renderChunks(deps) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (["react", "react-router-dom", "react-dom", "react-icons"].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
+// function renderChunks(deps) {
+//   let chunks = {};
+//   Object.keys(deps).forEach((key) => {
+//     if (["react", "react-router-dom", "react-dom", "react-icons"].includes(key)) return;
+//     chunks[key] = [key];
+//   });
+//   return chunks;
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-router-dom", "react-dom", "react-icons"],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
-  },
+  // build: {
+  //   sourcemap: false,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ["react", "react-router-dom", "react-dom", "react-icons"],
+  //         ...renderChunks(dependencies),
+  //       },
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
